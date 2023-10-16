3b776c0a8bee97f22b6eb6b9cfeb340a1bc568c7
FengTianShi <fts2470216@gmail.com>
2023-10-16 21:54:54 +0900

    Backend init

---
+++ OrderSmoothAPI/src/main/java/com/nobody/OrderSmoothAPI/OrderSmoothApiApplication.java
@@ -0,0 +1,13 @@
+package com.nobody.OrderSmoothAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSmoothApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSmoothApiApplication.class, args);
+	}
+
+}
