7e0d13fd98ecd67be9d07c8a5c8ae41a6d225985
FengTianShi <fts2470216@gmail.com>
2023-10-23 02:34:02 +0900

    前端设计

---
+++ ordersmooth-employee/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <AppHeader></AppHeader>
+    <v-main>
+      <v-container fluid class="pa-1">
+      </v-container>
+    </v-main>
+    <AppFooter></AppFooter>
+  </v-layout>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+}
+</script>
---
+++ ordersmooth-employee/src/components/AppFooter.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-app-bar location="bottom">
+        <div class="ml-3 text-caption">
+            Total :<br>$ 9999999
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn size="small" stacked @click="ShowReceiptInfo = true">
+            <v-icon icon="mdi-receipt-clock-outline"></v-icon>
+            Receipt
+        </v-btn>
+
+        <v-btn size="small" stacked @click="ShowCartInfo = true">
+            <v-badge color="error" content="2">
+                <v-icon icon="mdi-cart-outline"></v-icon>
+            </v-badge>
+            Cart
+        </v-btn>
+
+        <v-btn size="small" stacked @click="ShowOrderConfirm = true">
+            <v-icon icon="mdi-room-service-outline"></v-icon>
+            Order
+        </v-btn>
+    </v-app-bar>
+
+    <ReceiptInfo v-model="ShowReceiptInfo" @closed="ShowReceiptInfo = false" />
+    <CartInfo v-model="ShowCartInfo" @closed="ShowCartInfo = false" />
+    <OrderConfirm v-model="ShowOrderConfirm" @closed="ShowOrderConfirm = false" />
+</template>
+
+<script>
+
+export default {
+    components: {
+    }, data() {
+        return {
+        }
+    }
+}
+</script>
